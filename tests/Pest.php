aa5f716ce53f586db2e7ce45bf60cf228dd3ebe5
Tuan Anh Lê <vesviet@gmail.com>
2026-08-12 21:09:07 +0700

    fix(tests): apply RefreshDatabase to Unit tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,5 +3,4 @@
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-uses(TestCase::class)->in('Feature', 'Unit');
-uses(RefreshDatabase::class)->in('Feature');
+uses(TestCase::class, RefreshDatabase::class)->in('Feature', 'Unit');
